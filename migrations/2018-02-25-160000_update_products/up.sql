3e4524080e46eed17776ef297d84e202030cb3f6
serejkaaa512 <5125402@mail.ru>
2018-02-27 16:15:41 +0300

    drop currencies table

--- migrations/2018-02-25-160000_update_products/up.sql
+++ migrations/2018-02-25-160000_update_products/up.sql
@@ -1,3 +1,5 @@
 ALTER TABLE products DROP COLUMN default_language;
 ALTER TABLE products ADD COLUMN language_id INTEGER NOT NULL DEFAULT 1;
 ALTER TABLE products DROP COLUMN category;
+ALTER TABLE stores DROP COLUMN currency_id;
+ALTER TABLE stores ADD COLUMN currency_id INTEGER NOT NULL DEFAULT 1;

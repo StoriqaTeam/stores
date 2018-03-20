efd1315720c8d054e714adaa06f3b434a00670e8
Sergey Efremov <5125402@mail.ru>
2018-03-20 17:56:20 +0300

    added default categories migration

---
+++ migrations/2018-03-20-180000_update_categories/up.sql
@@ -0,0 +1,38 @@
+DELETE FROM prod_attr_values;
+DELETE FROM products;
+DELETE FROM base_products;
+DROP TABLE categories CASCADE;
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name JSONB NOT NULL,
+    meta_field VARCHAR,
+    parent_id INTEGER
+);
+
+CREATE UNIQUE INDEX stores_categories_id_idx ON categories (id);
+
+INSERT INTO categories (name, meta_field, parent_id) VALUES
+('[{"lang": "en", "text": "Jewelry & Accessories"}, {"lang": "ru", "text": "Ювелирные изделия и аксессуары"}]' ,NULL	,NULL),
+('[{"lang": "en", "text": "Clothing & Shoes"}, {"lang": "ru", "text": "Одежда и обувь"}]' ,NULL	,NULL),
+('[{"lang": "en", "text": "Accessories"}, {"lang": "ru", "text": "Аксессуары"}]' ,NULL,	1),
+('[{"lang": "en", "text": "Bags & purses"}, {"lang": "ru", "text": "Сумки и чехлы"}]' ,NULL,	1),
+('[{"lang": "en", "text": "Necklaces"}, {"lang": "ru", "text": "Ожерелья"}]' ,NULL,	1),
+('[{"lang": "en", "text": "Bracelets"}, {"lang": "ru", "text": "Браслеты"}]' ,NULL,	1),
+('[{"lang": "en", "text": "Womens"}, {"lang": "ru", "text": "Женский"}]' ,NULL,	2),
+('[{"lang": "en", "text": "Mens"}, {"lang": "ru", "text": "Мужской"}]' ,NULL,	2),
+('[{"lang": "en", "text": "Hair Accessories"}, {"lang": "ru", "text": "Аксессуары для волос"}]' ,NULL,	3),
+('[{"lang": "en", "text": "Sunglasses & Eyewear"}, {"lang": "ru", "text": "Солнцезащитные очки и очки"}]' ,NULL,	3),
+('[{"lang": "en", "text": "Belts & Suspenders"}, {"lang": "ru", "text": "Ремни и подтяжки"}]' ,NULL,	3),
+('[{"lang": "en", "text": "Keychains & Lanyards"}, {"lang": "ru", "text": "Брелки и ремешки"}]' ,NULL,	3),
+('[{"lang": "en", "text": "Umbrellas & Rain Accessories"}, {"lang": "ru", "text": "Зонтики и аксессуары"}]' ,NULL,	3),
+('[{"lang": "en", "text": "Backpacks"}, {"lang": "ru", "text": "Рюкзаки"}]' ,NULL,	4),
+('[{"lang": "en", "text": "Phone Case"}, {"lang": "ru", "text": "Чехол для телефона"}]' ,NULL,	4),
+('[{"lang": "en", "text": "Beaded Bracelets"}, {"lang": "ru", "text": "Браслеты из бисера"}]' ,NULL,	6),
+('[{"lang": "en", "text": "Cuff Bracelets"}, {"lang": "ru", "text": "Браслеты с манжетами"}]' ,NULL,	6),
+('[{"lang": "en", "text": "Dresses"}, {"lang": "ru", "text": "Платья"}]' ,NULL,	7),
+('[{"lang": "en", "text": "Jackets & Coats"}, {"lang": "ru", "text": "Куртки и пальто"}]' ,NULL,	7),
+('[{"lang": "en", "text": "Shirts"}, {"lang": "ru", "text": "Рубашки"}]' ,NULL,	8),
+('[{"lang": "en", "text": "Sweaters"}, {"lang": "ru", "text": "Свитера"}]' ,NULL,	8),
+('[{"lang": "en", "text": "Chains"}, {"lang": "ru", "text": "Цепочки"}]' ,NULL,	5),
+('[{"lang": "en", "text": "Pendants"}, {"lang": "ru", "text": "Подвески"}]' ,NULL,	5);

4063f5be153933f5df15ad86abaef29c179d6173
Sergey Efremov <5125402@mail.ru>
2018-04-03 11:02:07 +0300

    changed returning search options

--- src/models/attributes/attribute_filter.rs
+++ src/models/attributes/attribute_filter.rs
@@ -5,13 +5,33 @@ pub struct AttributeFilter {
     pub range: Option<RangeFilter>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Default, Serialize, Deserialize, Clone, Debug, Hash, PartialEq)]
 pub struct EqualFilter {
     pub values: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Default, Serialize, Deserialize, Clone, PartialEq, Debug)]
 pub struct RangeFilter {
     pub min_value: Option<f64>,
     pub max_value: Option<f64>,
+}
+
+impl RangeFilter {
+    pub fn add_value(&mut self, value: f64) {
+        if let Some(min) = self.min_value {
+            if value < min {
+                self.min_value = Some(value);
+            }
+        } else {
+            self.min_value = Some(value)
+        }
+
+        if let Some(max) = self.max_value {
+            if value > max {
+                self.max_value = Some(value);
+            }
+        } else {
+            self.max_value = Some(value)
+        }
+    }
 }

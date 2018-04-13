925f063f62ddf1896031639a964b71c6ff69c534
Sergey Efremov <5125402@mail.ru>
2018-04-13 17:19:14 +0300

    fixed categories cache

--- src/repos/categories/mod.rs
+++ src/repos/categories/mod.rs
@@ -59,7 +59,6 @@ impl<'a, T: Connection<Backend = Pg, TransactionManager = AnsiTransactionManager
     fn find(&self, id_arg: i32) -> RepoResult<Category> {
         debug!("Find in categories with id {}.", id_arg);
         let query = categories.filter(id.eq(id_arg));
-
         query
             .first::<RawCategory>(self.db_conn)
             .map_err(Error::from)
@@ -143,7 +142,6 @@ impl<'a, T: Connection<Backend = Pg, TransactionManager = AnsiTransactionManager
                 let mut result: Category = updated_category.into();
                 let children = create_tree(&cats, Some(id_arg));
                 result.children = children;
-                self.cache.set(result.clone());
                 Ok(result)
             })
     }
@@ -163,8 +161,11 @@ impl<'a, T: Connection<Backend = Pg, TransactionManager = AnsiTransactionManager
                     let mut root = Category::default();
                     let children = create_tree(&cats, None);
                     root.children = children;
-                    self.cache.set(root.clone());
                     Ok(root)
+                })
+                .and_then(|cat| {
+                    self.cache.set(cat.clone());
+                    Ok(cat)
                 })
         }
     }

c769a9e2b6fd719ade7f7edec6ee1ce34d2fea29
Serejka <5125402@mail.ru>
2018-05-08 15:18:23 +0300

    updated exchange mutation

--- migrations/2018-05-07-144000_update_currency_exchange/up.sql
+++ migrations/2018-05-07-144000_update_currency_exchange/up.sql
@@ -1,10 +1,10 @@
-CREATE UNIQUE INDEX currency_exchange_id_idx ON currency_exchange (id);
+CREATE UNIQUE INDEX IF NOT EXISTS currency_exchange_id_idx ON currency_exchange (id);
 
 INSERT INTO currency_exchange (rouble, euro, dollar, bitcoin, etherium, stq) VALUES (
-    '{ "rouble" : 1.0, "euro" : 2.0, "dollar" : 3.0, "bitcoin" : 4.0, "etherium" : 5.0, "stq" : 6.0 }',
-    '{ "rouble" : 0.5, "euro" : 1.0, "dollar" : 3.0, "bitcoin" : 4.0, "etherium" : 5.0, "stq" : 6.0 }',
-    '{ "rouble" : 0.5, "euro" : 0.5, "dollar" : 1.0, "bitcoin" : 4.0, "etherium" : 5.0, "stq" : 6.0 }',
-    '{ "rouble" : 0.5, "euro" : 7.0, "dollar" : 3.0, "bitcoin" : 1.0, "etherium" : 5.0, "stq" : 6.0 }',
-    '{ "rouble" : 0.5, "euro" : 8.0, "dollar" : 3.0, "bitcoin" : 4.0, "etherium" : 1.0, "stq" : 6.0 }',
-    '{ "rouble" : 0.5, "euro" : 9.0, "dollar" : 3.0, "bitcoin" : 4.0, "etherium" : 5.0, "stq" : 1.0 }'
+    '{ "rouble" : 1.0, "euro" : 75.0, "dollar" : 63.0, "bitcoin" : 500000.0, "etherium" : 66666.0, "stq" : 5.5 }',
+    '{ "rouble" : 0.013, "euro" : 1.0, "dollar" : 1.1, "bitcoin" : 7000.0, "etherium" : 1000.0, "stq" : 0.01 }',
+    '{ "rouble" : 0.016, "euro" : 0.9, "dollar" : 1.0, "bitcoin" : 8000.0, "etherium" : 1200.0, "stq" : 0.013 }',
+    '{ "rouble" : 0.000002, "euro" : 0.00014, "dollar" : 0.00012, "bitcoin" : 1.0, "etherium" : 0.01, "stq" : 0.000001 }',
+    '{ "rouble" : 0.00015, "euro" : 0.001, "dollar" : 0.0008, "bitcoin" : 100.0, "etherium" : 1.0, "stq" : 0.0001 }',
+    '{ "rouble" : 0.18, "euro" : 100.0, "dollar" : 80.0, "bitcoin" : 100000.0, "etherium" : 10000.0, "stq" : 1.0 }'
 );

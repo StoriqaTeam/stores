c7ad01151e22f4c9130a30377236ed9049f6ad02
Alexander Andreev <andreevlex.as@gmail.com>
2018-08-24 16:30:03 +0300

    added to_http_config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 //! Config module contains the top-level config for the app.
 use std::env;
 
+use stq_http;
 use stq_logging::GrayLogConfig;
 
 use config_crate::{Config as RawConfig, ConfigError, Environment, File};
@@ -52,5 +53,13 @@ impl Config {
         s.merge(Environment::with_prefix("STQ_USERS"))?;
 
         s.try_into()
+    }
+
+    pub fn to_http_config(&self) -> stq_http::client::Config {
+        stq_http::client::Config {
+            http_client_buffer_size: self.client.http_client_buffer_size,
+            http_client_retries: self.client.http_client_retries,
+            timeout_duration_ms: self.client.http_timeout_ms,
+        }
     }
 }
--- src/services/base_products.rs
+++ src/services/base_products.rs
@@ -1121,11 +1121,7 @@ pub mod tests {
         let cpu_pool = CpuPool::new(1);
 
         let config = Config::new().unwrap();
-        let http_config = HttpConfig {
-            http_client_retries: config.client.http_client_retries,
-            http_client_buffer_size: config.client.http_client_buffer_size,
-            timeout_duration_ms: config.client.http_timeout_ms,
-        };
+        let http_config = config.to_http_config();
         let client = stq_http::client::Client::new(&http_config, &handle);
         let client_handle = client.handle();
 

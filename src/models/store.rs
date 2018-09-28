3f086571926789dc2fe8345ac9948987933ca82d
Alexander Andreev <andreevlex.as@gmail.com>
2018-09-28 11:47:07 +0300

    fix search moderator search

--- src/models/store.rs
+++ src/models/store.rs
@@ -274,6 +274,6 @@ impl StoreWithBaseProducts {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ModeratorStoreSearchTerms {
     pub name: Option<String>,
-    pub store_manager_id: Option<UserId>,
+    pub store_manager_ids: Vec<UserId>,
     pub state: Option<ModerationStatus>,
 }

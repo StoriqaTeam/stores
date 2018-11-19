b01879a003aa9575dbe382110de580b1fd225441
busarovalex <a.busarov@storiqa.com>
2018-11-19 12:07:58 +0300

    migration fix

--- migrations/2019-11-16-134133_populate_attribute_values/up.sql
+++ migrations/2019-11-16-134133_populate_attribute_values/up.sql
@@ -1,5 +1,3 @@
-CREATE UNIQUE INDEX IF NOT EXISTS stores_attribute_values_attr_id_code_idx ON attribute_values (attr_id, code);
-
 insert into attribute_values (attr_id, code, translations)
 
 select
@@ -19,3 +17,5 @@ update attribute_values set code=(
 ) where code='';
 
 update prod_attr_values set attr_value_id=(select av.id from attribute_values av where av.attr_id=prod_attr_values.attr_id and av.code=value);
+
+CREATE UNIQUE INDEX IF NOT EXISTS stores_attribute_values_attr_id_code_idx ON attribute_values (attr_id, code);

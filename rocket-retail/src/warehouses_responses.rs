b334122a3c8667e6b148b846dc6b927ea3fba871
Serejka <5125402@mail.ru>
2019-03-06 10:12:40 +0300

    fixed rocket retail to warehouses request

--- rocket-retail/src/warehouses_responses.rs
+++ rocket-retail/src/warehouses_responses.rs
@@ -1,7 +1,6 @@
 use stq_types::*;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct CatalogStocksResponse {
     pub id: StockId,
     pub warehouse_id: WarehouseId,
